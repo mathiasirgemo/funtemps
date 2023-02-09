c04948e9aa82661176d5100945a2ac37f8113048
mathiasirgemo <mathiasirgemo1@gmail.com>
2023-02-09 13:24:25 +0100

    gjort ferdif funfacts

--- funfacts/funfacts.go
+++ funfacts/funfacts.go
@@ -1,14 +1,36 @@
 package funfacts
 
-/**
-  Implementer funfacts-funksjon:
-    GetFunFacts(about string) []string
-      hvor about kan ha en av tre testverdier, -
-        sun, luna eller terra
-  Sett inn alle Funfucts i en struktur
-  type FunFacts struct {
-      Sun []string
-      Luna []string
-      Terra []string
-  }
-*/
+type FunFact struct {
+	Sun   []string
+	Luna  []string
+	Terra []string
+}
+
+func GetFunFacts(about string) []string {
+	funFacts := FunFact{
+		Sun: []string{
+			"Temperatur i Solens kjerne	15 000 000C°",
+			"Temperatur på ytre lag av Solen	5778K",
+		},
+		Luna: []string{
+			"Temperatur på Månens overflate om natten	-183°C",
+			"Temperatur på Månens overflate om dagen",
+		},
+		Terra: []string{
+			"Høyeste temperatur målt på Jordens overflate	56.7°C",
+			"Laveste temperatur målt på Jordens overflate	-89,4°C",
+			"Temperatur i Jordens indre kjerne	9392K",
+		},
+	}
+
+	switch about {
+	case "Sun":
+		return funFacts.Sun
+	case "luna":
+		return funFacts.Luna
+	case "terra":
+		return funFacts.Terra
+	default:
+		return []string{"Ingen funfacts funnet.."}
+	}
+}
